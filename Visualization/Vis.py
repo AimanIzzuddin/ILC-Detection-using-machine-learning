40c7c3ffd09dd4bb7a8fe9590184cb5424e1eebe
unknown <aimanizzuddin567@gmail.com>
2025-06-07 01:37:03 +0800

    initial commit

---
+++ Visualization/Vis.py
@@ -0,0 +1,106 @@
+import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+from tensorflow.keras.preprocessing import image
+
+breast_imgs = glob.glob('/kaggle/input/breast-histopathology-images/IDC_regular_ps50_idx5/**/*.png', recursive = True)
+for imgname in breast_imgs[:5]:
+    print(imgname)
+
+non_cancer_imgs = []
+cancer_imgs = []
+
+for img in breast_imgs:
+    if img[-5] == '0' :
+        non_cancer_imgs.append(img)
+    
+    elif img[-5] == '1' :
+        cancer_imgs.append(img)
+
+non_cancer_num = len(non_cancer_imgs)  # No cancer
+cancer_num = len(cancer_imgs)   # Cancer 
+        
+total_img_num = non_cancer_num + cancer_num
+        
+print('Number of Images of no cancer: {}' .format(non_cancer_num))   # images of Non cancer
+print('Number of Images of cancer : {}' .format(cancer_num))   # images of cancer 
+print('Total Number of Images : {}' .format(total_img_num))
+
+data_insight_1 = pd.DataFrame({'state of cancer' : ['0','1'],'Numbers of Patients' : [198738,78786]})
+
+bar = px.bar(data_frame=data_insight_1, x = 'state of cancer', y='Numbers of Patients', color='state of cancer')
+bar.update_layout(title_text='Number of Patients with cancer (1) and patients with no cancer (0)', title_x=0.5)
+bar.show()
+
+r= pd.DataFrame(dicom_cleaned_data['SeriesDescription'].value_counts())
+r= r.reset_index()
+r= r.rename(columns={'SeriesDescription':'SeriesDescription_counts', 'index':'SeriesDescription'})
+r
+
+ba_1 = px.bar(data_frame=dicom_cleaned_data, x='SeriesDescription', color='SeriesDescription',
+              title='Types of images used')
+ba_1.update_layout(title_text='Types of images used', title_x=0.5)
+ba_1.show()
+
+f= pd.DataFrame(dicom_cleaned_data['BodyPartExamined'].value_counts())
+f= f.reset_index()
+f= f.rename(columns={'BodyPartExamined':'BodyPartExamined_counts', 'index':'Breast part Examined'})
+f
+
+ba = px.bar(data_frame=f, x="BodyPartExamined_counts", y="count", color="BodyPartExamined_counts")
+ba.update_layout(title_text='Breast Parts Examined', title_x=0.5, yaxis=dict(type='log'))
+ba.show()
+
+data_insight_2 = pd.DataFrame({'abnormality':[Data_cleaning_1.abnormality_type[0],Data_cleaning_2.abnormality_type[0]],
+                               'counts_of_abnormalties':[len(Data_cleaning_1),len(Data_cleaning_2)]})
+data_insight_2
+
+bar_2 =px.bar(data_frame=data_insight_2, x = 'abnormality', y='counts_of_abnormalties', color = 'abnormality')
+bar_2.update_layout(title_text='Types of abnormalites of cancer', title_x=0.5)
+bar_2.show()
+
+x = Data_cleaning_1.left_or_right_breast.value_counts().RIGHT
+y = Data_cleaning_1.left_or_right_breast.value_counts().LEFT
+print(x,y)
+
+data_insight_3 = pd.DataFrame({'left_or_right_breast':['RIGHT','LEFT'] , 'Counts':[x,y]})
+data_insight_3
+
+insight_3 = px.bar(data_insight_3, y= 'Counts', x='left_or_right_breast',color = 'left_or_right_breast')
+insight_3.update_layout(title_text=' clacification cancer position of the breasts ', title_x=0.5)
+insight_3.show()
+
+z = pd.DataFrame(Data_cleaning_1['calc_type'].value_counts())
+z = z.reset_index()
+z= z.rename(columns={'calc_type':'calc_type_counts'})
+z
+
+plt.figure(figsize = (15, 15))
+
+some_non = np.random.randint(0, len(non_cancer_imgs), 18)
+some_can = np.random.randint(0, len(cancer_imgs), 18)
+
+s = 0
+for num in some_non:
+    
+        img = image.image_utils.load_img((non_cancer_imgs[num]), target_size=(100, 100))
+        img = image.image_utils.img_to_array(img)
+        
+        plt.subplot(6, 6, 2*s+1)
+        plt.axis('off')
+        plt.title('no cancer')
+        plt.imshow(img.astype('uint8'))
+        s += 1
+        
+s = 1
+for num in some_can:
+    
+        img = image.image_utils.load_img((cancer_imgs[num]), target_size=(100, 100))
+        img = image.image_utils.img_to_array(img)
+        plt.subplot(6, 6, 2*s)
+        plt.axis('off')        
+        plt.title('cancer')
+        plt.imshow(img.astype('uint8'))
+        s += 1
